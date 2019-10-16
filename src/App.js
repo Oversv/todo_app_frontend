1d268b71e085b1156bb23c350d8f0e7db8c3c35d
Alberto MDV <albertomdv@gmail.com>
2019-10-16 12:10:34 +0100

    Learning components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Intro from './components/Intro';
+
+function App() {
+    return (
+
+        <
+        div >
+        <
+        Header / >
+        <
+        Intro / >
+
+        <
+        /div> 
+
+
+    );
+}
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Intro extends React.Component {
+    render() {
+        return ( <
+            p > Welcome to my new app < /p>
+
+        )
+    }
+}
+
+export default Intro;

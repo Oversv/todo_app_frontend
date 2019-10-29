0b0a194c1966714e09c3203c2d7ba8a79b0e3328
Alberto MDV <albertomdv@gmail.com>
2019-10-29 17:39:50 +0000

    Updates

---
+++ precache-manifest.789a8c253169213327d6682b5c2ab412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f74186dd971dd5993fbc80c37df8f4c",
+    "url": "/todo_app_frontend/index.html"
+  },
+  {
+    "revision": "b4c4eb783471d2d055af",
+    "url": "/todo_app_frontend/static/css/main.88e0136e.chunk.css"
+  },
+  {
+    "revision": "97f82b32c60bcb1c64d8",
+    "url": "/todo_app_frontend/static/js/2.8db9aff1.chunk.js"
+  },
+  {
+    "revision": "b4c4eb783471d2d055af",
+    "url": "/todo_app_frontend/static/js/main.68cc609f.chunk.js"
+  },
+  {
+    "revision": "3fe3d5b967a993183ded",
+    "url": "/todo_app_frontend/static/js/runtime-main.055414d6.js"
+  }
+]);
